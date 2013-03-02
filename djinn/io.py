119a851377e69e83d2e22b0fc9ce4fd398d9ed17
Nic Roland <nicroland9@gmail.com>
2013-03-02 23:23:05 +0000

    Some refactoring in io.py

--- djinn/io.py
+++ djinn/io.py
@@ -1,28 +1,35 @@
 import pygame
+from inspect import isfunction
 
 class DjinnKeyboard(object):
     _key_handlers = {}
-    _key_states = [] # TODO: refactor: _active_keys
+    _active_keys = []
 
     def register_keys(self):
         raise NotImplementedError()
 
-    # TODO: add a register method
+    def _register(self, key, f):
+        if isinstance(f, tuple) and len(f) == 2:
+            self._key_handlers[key] = f
+        elif isfunction(f):
+            self._key_handlers[key] = f
+        else:
+            raise AttributeError()
 
     def _update_keys(self):
         keys = pygame.key.get_pressed()
 
         for key, state in enumerate(keys):
-            if state and key not in self._key_states:
+            if state and key not in self._active_keys:
                 # Handle key
-                self._key_states.append(key)
+                self._active_keys.append(key)
                 if key in self._key_handlers:
                     handle = self._key_handlers[key]
                     if isinstance(handle, tuple) and len(handle) == 2:
                         self.player.accelerate(*handle)
-            elif not state and key in self._key_states:
+            elif not state and key in self._active_keys:
                 # Undo key
-                self._key_states.remove(key)
+                self._active_keys.remove(key)
                 if key in self._key_handlers:
                     handle = self._key_handlers[key]
                     if isinstance(handle, tuple) and len(handle) == 2:
@@ -31,10 +38,10 @@ class DjinnKeyboard(object):
 
 class BasicKeyboard(DjinnKeyboard):
     def register_keys(self):
-        self._key_handlers = {pygame.K_UP: (0, -1),
-        pygame.K_DOWN: (0, 1),
-        pygame.K_LEFT: (-1, 0),
-        pygame.K_RIGHT: (1, 0)}
+        self._register(pygame.K_UP, (0, -1))
+        self._register(pygame.K_DOWN, (0, 1))
+        self._register(pygame.K_LEFT, (-1, 0))
+        self._register(pygame.K_RIGHT, (1, 0))
 
 class DjinnMouse(object):
     def process_mouse(self, event):
