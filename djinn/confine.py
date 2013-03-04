d71209f67b4c0cb68993811b0c14ab8c5f82f806
Nic Roland <nicroland9@gmail.com>
2013-03-04 20:37:58 +0000

    Changed DjSprite to DjinnSprite, removed redundant coords attribute.

--- djinn/confine.py
+++ djinn/confine.py
@@ -3,31 +3,37 @@ class DjinnConfine(object):
         raise NotImplementedError
 
     def exited(self):
+        area = self.screen.get_size()
+
         for z in [X, Y]:
-            if self.coords[z] > self.area[z]:
+            if self.rect[z] > area[z]:
                 return True
-            if self.coords[z] < 0:
+            if self.rect[z] < 0:
                 return True
         return False
 
 class BrickWallConfine(DjinnConfine):
     def calculate(self):
-        last_coords = self.coords
+        last_coords = self.rect[:2]
 
         self._calculate()
 
         if self.exited():
-            self.coords = last_coords
+            # Block from exiting by keeping coords the same
+            self.rect[:2] = last_coords
 
 class BounceConfine(DjinnConfine):
     def calculate(self):
         self._calculate()
+
+        bounds = sub_lists(self.screen.get_size(), self.rect[2:]
+
         for z in [X, Y]:
-            if self.coords[z] > self.bounds[z]:
-                self.coords[z] -= self.coords[z] - self.bounds[z]
+            if self.rect[z] > bounds[z]:
+                self.rect[z] -= self.rect[z] - bounds[z]
                 self.velocity[z] *= -1
-            if self.coords[z] < 0:
-                self.coords[z] *= -1
+            if self.rect[z] < 0:
+                self.rect[z] *= -1
                 self.velocity[z] *= -1
 
 class AlertConfine(DjinnConfine):
--- djinn/sprite.py
+++ djinn/sprite.py
@@ -2,11 +2,10 @@ from utils import mul_lists, add_lists, sub_lists
 import pygame
 from os.path import isfile
 
-class DjSprite(pygame.sprite.Sprite):
+class DjinnSprite(pygame.sprite.Sprite):
     def __init__(self, screen, res, coords, size=[16, 16]):
-        super(DjSprite, self).__init__()
+        super(DjinnSprite, self).__init__()
         self.screen = screen
-        self.coords = coords
         self.size = size
         self.velocity = [0, 0]
         self.speed = [1, 1]
@@ -25,6 +24,7 @@ class DjSprite(pygame.sprite.Sprite):
             raise Exception('Faulty res')
 
         self.rect = self.image.get_rect()
+        self.rect[:2] = coords
 
     # Movement
     def accelerate(self, x, y):
@@ -47,7 +47,7 @@ class DjSprite(pygame.sprite.Sprite):
 
     def set_position(self, x, y):
         inp = [x, y]
-        position = self.coords
+        position = self.rect[:2]
 
         if self.grid():
             inp = mul_lists(inp, self.grid_size)
@@ -56,7 +56,7 @@ class DjSprite(pygame.sprite.Sprite):
         self.delta = sub_lists(inp, position)
 
     def _apply_position(self):
-        self.coords = add_lists(self.coords, self.delta)
+        self.rect[:2] = add_lists(self.rect[:2], self.delta)
         self.delta = [0, 0]
 
     def grid(self):
@@ -76,69 +76,3 @@ class DjSprite(pygame.sprite.Sprite):
 
 class DjinnGroup(pygame.sprite.Group):
     pass
-
-# Base classes
-class DjinnSprite(object):
-    grid_size = [1, 1]
-    speed = [1, 1]
-    delta = [0, 0]
-
-    def __init__(self, screen, colour, coords, size, velocity):
-        self.screen = screen
-        self.colour = colour
-        self.coords = mul_lists(
-                coords,
-                self.grid_size)
-        self.size = size
-        self.velocity = velocity
-
-        self.area = self.screen.get_size()
-        self.bounds = sub_lists(self.area, self.size)
-
-    # Movement
-    def accelerate(self, x, y):
-        self.velocity = add_lists(self.velocity, [x, y])
-
-    def set_velocity(self, x, y):
-        self.velocity = [x, y]
-
-    def _apply_velocity(self):
-        self.move(*self.velocity)
-
-    def move(self, x, y):
-        inp = mul_lists([x, y], self.speed)
-        if self.grid():
-            inp = mul_lists(inp, self.grid_size)
-
-        self.delta = add_lists(
-                self.delta,
-                inp)
-
-    def set_position(self, x, y):
-        inp = [x, y]
-        position = self.coords
-
-        if self.grid():
-            inp = mul_lists(inp, self.grid_size)
-            position = mul_lists(position, self.grid_size)
-
-        self.delta = sub_lists(inp, position)
-
-    def _apply_position(self):
-        self.coords = add_lists(self.coords, self.delta)
-        self.delta = [0, 0]
-
-    def grid(self):
-        return self.grid_size != [1, 1]
-
-    # Calculate final position
-    def calculate(self):
-        self._calculate()
-
-    def _calculate(self):
-        self._apply_velocity()
-        self._apply_position()
-
-    # Draw on screen
-    def draw(self):
-        raise NotImplementedError()
