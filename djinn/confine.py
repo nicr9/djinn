b2c39f93cd7439414b26f0d27d4fbda217fc68a4
Nic Roland <nicroland9@gmail.com>
2013-03-05 00:16:51 +0000

    Added DonutConfine and fixed two bugs in confine.py

--- djinn/confine.py
+++ djinn/confine.py
@@ -1,3 +1,5 @@
+from utils import X, Y
+
 class DjinnConfine(object):
     def calculate(self, f):
         raise NotImplementedError
@@ -11,6 +13,37 @@ class DjinnConfine(object):
             if self.rect[z] < 0:
                 return True
         return False
+
+    def exited_via(self):
+        area = self.screen.get_size()
+
+        # w = 1, n = 2, e = 3, s = 4
+        for z in [X, Y]:
+            if self.rect[z] > area[z]:
+                return z + 3
+            if self.rect[z] < 0:
+                return z + 1
+
+        # hasn't left the screen
+        return 0
+
+class DonutConfine(DjinnConfine):
+    def calculate(self):
+        last_coords = self.rect[:2]
+
+        self._calculate()
+
+        exit = self.exited_via()
+        area = self.screen.get_size()
+
+        if exit == 1:
+            self.rect[0] = area[0]
+        elif exit == 2:
+            self.rect[1] = area[1]
+        elif exit == 3:
+            self.rect[0] = 0
+        elif exit == 4:
+            self.rect[1] = 0
 
 class BrickWallConfine(DjinnConfine):
     def calculate(self):
@@ -26,7 +59,7 @@ class BounceConfine(DjinnConfine):
     def calculate(self):
         self._calculate()
 
-        bounds = sub_lists(self.screen.get_size(), self.rect[2:]
+        bounds = sub_lists(self.screen.get_size(), self.rect[2:])
 
         for z in [X, Y]:
             if self.rect[z] > bounds[z]:
