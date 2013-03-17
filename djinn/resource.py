ffe7850d2282934883d18a918b091286eaacbe52
Nic Roland <nicroland9@gmail.com>
2013-03-17 15:03:25 +0000

    Cleaned up djinn/resource.py

--- djinn/resource.py
+++ djinn/resource.py
@@ -2,12 +2,18 @@ import os, re, json, pygame
 from utils import Colours
 
 class Resources(object):
+    # Regexs
     res_re = re.compile('(.*)\.dj$')
-    wnes = ['west', 'north', 'east', 'south']
+    face_re = "%s_(.*).bmp"
+
+    # _res[resource_name][face_number] -> pygame.Surface
     _res = {}
 
     def __init__(self, screen, res_path):
+        # Stash the screen surface
         self.screen = screen
+
+        # Walk the provided path looking for .dj files
         for root, dirs, files in os.walk(res_path):
             match = re.search(self.res_re, root)
             if match:
@@ -15,22 +21,21 @@ class Resources(object):
                 res_name = os.path.basename(match.group(1))
                 self._res[res_name] = {}
 
-                # Open each .bmp in .dj
-                wnes_re = "%s_([1-4]).bmp"
+                # Each .bmp in the .dj represents a different 'face'
                 for f in files:
-                    bmp_ex = wnes_re % res_name
-                    direction_file = re.match(bmp_ex, f)
-                    if direction_file:
-                        facing = direction_file.group(1)
+                    bmp_ex = self.face_re % res_name
+                    face_file = re.match(bmp_ex, f)
+                    if face_file:
+                        face_varient = face_file.group(1)
                         full_path = os.path.join(root, f)
                         temp = pygame.image.load(full_path).convert()
                         temp.set_colorkey(Colours.black)
-                        self._res[res_name][int(facing)] = temp
+                        self._res[res_name][int(face_varient)] = temp
             else:
                 continue
 
-            for key, val in self._res.iteritems():
-                print "%s: %s" % (key, val)
+        for key, val in self._res.iteritems():
+            print "%s: %s" % (key, val)
 
     def __getitem__(self, key):
         try:
