51a3e46944231c8629740e815bea0752125e6663
Nic Roland <nicroland9@gmail.com>
2014-05-27 07:50:41 +0100

    Initial setup.py file + added build/ to .gitignore.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='djinn',
+    version='v0.1',
+    author='Nic Roland',
+    author_email='nicroland9@gmail.com',
+    packages=['djinn'],
+    description='A quick and easy way to write games.',
+    #long_description=open('README.rst').read(),
+)
